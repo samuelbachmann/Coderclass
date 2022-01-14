1446a96678f9a88b38cd59ee9fd002e00b292148
samuelbachmann <43247867+samuelbachmann@users.noreply.github.com>
2022-01-14 14:03:44 +0100

    Queens klaar

--- CPP/queens.cpp
+++ CPP/queens.cpp
@@ -1,66 +1,102 @@
 #include <iostream>
 #include <vector>
 
-int inputCheck(){
-    int input;
-    if(std::cin >> input){
-        if(input == 2 or input <= 0){
-            throw std::runtime_error("No solution found to place 2 queens on a 2 by 2 chess board");
-        }else{return input;}
-    }else{throw std::runtime_error("error: invalid input");}
+int showBoard(std::vector<std::vector<std::string> > nQueens){
+    for (int i = 0; i < nQueens.size(); i++) {
+        for (int j = 0; j < nQueens[i].size(); j++){
+            std::cout << nQueens[i][j];
+        }
+        std::cout << "\n";
+    }
+    return 0;
 }
 
-bool checkPlacement(std::vector <std::vector<bool>> &board, int N, int row, int lane){
-    row -= 1;
-    if(row == 0){
-        return true;
-    }else if(lane -1 < N and lane + 1 > N){
-        if(board.at(row).at(lane) and board.at(row).at(lane + 1) and board.at(row).at(lane - 1)){
-            return true;
-        }else{return false;}
-    }else if(lane -1 < N){
-        if(board.at(row).at(lane) and board.at(row).at(lane - 1)){
-            return true;
-    }else if(lane +1 < N){
-        if(board.at(row).at(lane) and board.at(row).at(lane + 1)){
-            return true;
+bool checkPosition(std::vector<std::vector<std::string> > nQueens, int row, int column, int N){
+    for (int i = 0; i < column; i++){
+        if (nQueens[row][i] == "Q"){
+            return false;
         }
     }
+
+   for (int i=row, j=column; i>=0 && j>=0; i--, j--){ 
+       if (nQueens[i][j] == "Q"){
+           return false;
+       }
+   }
+
+   for (int i=row, j=column; i>=0 && j<N; i--, j++){ 
+       if (nQueens[i][j] == "Q"){
+           return false;
+       }
+   }
+
+    for (int i = 0; i < N; i++){ 
+        if (nQueens[i][column] == "Q"){
+            return false;
+        }
+    }
+   return true;
 }
 
-bool placeQueens(int N, std::vector <std::vector<bool>> &board, int row){
-    bool possible = true;
-    int lane = 0;
-
-    // while(possible == true){
-    //     board.at(row).at(lane);
-    //     if(checkPlacement(board, N, row, lane) == false){
-    //         possible = false;
-    //     }else{lane++;}
-    // }
-
-    if(row != N){
-        // placeQueens(N, board, row++);
-        return false;
-    }else{return true;}
+bool solveNQueen(std::vector<std::vector<std::string> > nQueens, int N, int row){
+    if(row >= N){
+        showBoard(nQueens);
+        return true;
+    }
+    for( int k = 0; k < N; k++){
+        if (checkPosition(nQueens, row, k, N)){
+            nQueens[row][k] = "Q";
+            if (solveNQueen(nQueens, N, row + 1)){
+                return true;
+            }
+         nQueens[row][k] = ".";
+        }
+    }
+    return false;
 }
 
-int main(){
-    int amountQueens = 4;
+bool makeBoard(int N){
+    std::vector<std::vector<std::string> > nQueens;
+    for (int i = 0; i < N; i++){
+        std::vector<std::string> v1;
+        for (int j = 0; j < N; j++){ 
+            v1.emplace_back(".");
+        }
+        nQueens.push_back(v1);
+    }
+    if(!solveNQueen(nQueens, N, 0)) {
+      std::cout << "No solution found to place " << N << " queens on a " << N << " by " << N << " chess board";
+      return false;
+   }
+   
+   return true;
 
-    // std::cout << "How many queens to place on the board? ";
-    // try{
-    //     amountQueens = inputCheck();
-    // }catch(std::runtime_error& excpt){
-    //     std::cout << excpt.what() << std::endl;
-    //     return 1;
-    // }
+}
 
-    std::vector <bool> boardFalse(amountQueens, false);
-    std::vector <std::vector<bool>> board(amountQueens, boardFalse);
 
-    // placeQueens(amountQueens, board, 0);
+int readLine(){
 
-    checkPlacement(board, amountQueens, 0, 0);
+    int N = 2;
+    std::cout << "How many queens to place on the board? \n";
+
+    if (std::cin >> N && N > 0){
+        makeBoard(N);
+
+    }else{
+        throw std::runtime_error("invalid input");
+    }
+
+    return 0;
+}
+
+
+int main(){   
+    try{
+        readLine();
+    }
+    catch(std::runtime_error& excpt)
+     {
+         std::cout << "\nerror: " << excpt.what();
+     }
     return 0;
 }
