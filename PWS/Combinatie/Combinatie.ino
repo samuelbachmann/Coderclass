936d289dd6d4e65f7af1598fbdf283c7c65c39b5
samuelbachmann <samuel.bachmann@coderclass.nl>
2023-12-06 13:32:48 +0100

    Update Combinatie.ino

--- PWS/Combinatie/Combinatie.ino
+++ PWS/Combinatie/Combinatie.ino
@@ -1,9 +1,82 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MQ135.h>
+#include "TinyGPS++.h"
+#include <SoftwareSerial.h>
 
+#define PIN_MQ135 A2
+
+static const int RXPin = 0, TXPin = 1;
+static const uint32_t GPSBaud = 9600;
+
+MQ135 mq135_sensor(PIN_MQ135);
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
+
+float temperature = 21.0;
+float humidity = 25.0;
+
+void setup() {
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readMQ135();
+  readGPS();
+}
 
+void readMQ135() {
+  float rzero = mq135_sensor.getRZero();
+  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
+  float resistance = mq135_sensor.getResistance();
+  float ppm = mq135_sensor.getPPM();
+  float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
+
+  Serial.print("MQ135 RZero: ");
+  Serial.print(rzero);
+  Serial.print("\t Corrected RZero: ");
+  Serial.print(correctedRZero);
+  Serial.print("\t Resistance: ");
+  Serial.print(resistance);
+  Serial.print("\t PPM: ");
+  Serial.print(ppm);
+  Serial.print("\t Corrected PPM: ");
+  Serial.print(correctedPPM);
+  Serial.println("ppm");
+
+  delay(300);
+}
+
+void readGPS() {
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
+}
+
+void displayInfo() {
+  Serial.print(F("Location: "));
+  if (gps.location.isValid()) {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  } else {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date "));
+  if (gps.date.isValid()) {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  } else {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
